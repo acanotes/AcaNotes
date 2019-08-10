134d7ff7d44e7c240ef969420efee8708b16108a
StoneT2000 <stoneztao@gmail.com>
2019-08-10 14:36:39 -0700

    Revert "Initial Commit"
    This reverts commit b74c903435e95bd9d4b226baf417f4e7e1dddfa6.

--- public_html/login/login.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="../includes/login.inc.php">
-<input id="username" type="text" name="uid" placeholder="Username/email">
-<input id="password" type="password" name="pwd" placeholder="Password">
-<button id="submit" type="submit" name="submit">Login</button>
-</form>
