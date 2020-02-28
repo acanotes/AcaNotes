3b429a3297fa1d60bfa10fc46102efb6d98a2755
StoneT2000 <stoneztao@gmail.com>
2020-02-28 00:45:25 -0800

    Footer and contributors page

--- web/src/pages/Contributors/index.jsx
+++ web/src/pages/Contributors/index.jsx
@@ -4,16 +4,30 @@ import MainLayout from 'layouts/MainLayout';
 
 import './index.less';
 
-const TemplatePage = () => {
+const ContributorsPage = () => {
   return (
     <MainLayout>
-      <div className="TemplatePage">
+      <div className="ContributorsPage">
         <div className="main-container">
-        <h1>Acanotes</h1>
+        <h2>Founding Members</h2>
+        <p>Andrew Liu</p>
+        <p>Vincent Cai</p>
+        <p>Stone Tao</p>
+        <p>Tom Jiao</p>
+        <p>Alex Xu</p>
+        <p>Emma Liu</p>
+        <p>David Yei</p>
+        <br/>
+        <h2>Current management team</h2>
+        <br/>
+        <h2>Project contributors</h2>
+        <br/>
+        <h2>Honorary Members</h2>
+        <br/>
         </div>
       </div>
     </MainLayout>
   )
 }
 
-export default TemplatePage
+export default ContributorsPage
