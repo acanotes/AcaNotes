ee52adaa479e1194dfadb56860f66727c06be813
StoneT2000 <stoneztao@gmail.com>
2020-03-04 18:51:56 -0800

    Edit button and fotn changes

--- web/src/pages/ProfilePage/index.jsx
+++ web/src/pages/ProfilePage/index.jsx
@@ -3,7 +3,7 @@ import { Button } from 'antd';
 import MainLayout from 'layouts/MainLayout';
 import Avatar from 'components/User/Avatar';
 import NotesList from 'components/Notes/NotesList';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { getUser, getPopularUploads } from 'actions/users';
 
 import UserContext from 'UserContext.js';
@@ -16,16 +16,17 @@ const ProfilePage = () => {
   const userHooks = React.useContext(UserContext);
   const [profile, setProfile] = useState({});
   const [popularUploads, setPopularUploads] = useState([]);
+
+  const mine = params.id === userHooks.user.username;
+
   useEffect(() => {
 
     getUser(params.id).then((res) => {
       setProfile(res);
-      console.log(res);
     }).catch((error) => {
       errorLogger(error);
     })
     getPopularUploads(params.id).then((res) => {
-      console.log(res);
       setPopularUploads(res);
     }).catch(errorLogger)
   }, []);
@@ -37,14 +38,15 @@ const ProfilePage = () => {
             <Avatar size="large"/>
           </div>
           <h2>{profile.user_first} {profile.user_last}</h2>
-          <div className="title">Honorary Title: {profile.user_title}</div>
+          <div className="title">Honorary Title: <span>{profile.user_title}</span></div>
           <div className="rating">Rating: {profile.user_rating || 0}/5</div>
           <div className="downloads">User Downloads: {profile.user_downloads}</div>
-          <div className="desc">Description: {profile.user_description}</div>
+          <div className="desc">Description: <p>{profile.user_description || "User has no description"}</p></div>
           <div className="popular-uploads">
             <div className="title">Popular Uploads</div>
             {popularUploads ? <NotesList notes={popularUploads}/> : <p>This user has no uploads yet!</p>}
           </div>
+          { mine && <Link to={"/editProfile"}><Button size="large">Edit Profile</Button></Link> }
         </div>
       </div>
     </MainLayout>
