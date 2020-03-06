57ead79cb00bb12ee444cf882cbc163b54a17f89
StoneT2000 <stoneztao@gmail.com>
2020-03-05 21:45:32 -0800

    Create populate.php

---
+++ web/public/api/populate.php
@@ -0,0 +1,28 @@
+<?php
+  $env = getenv('PHP_ENV');
+  if ($env === "review") {
+    $url = getenv('DB_URL');
+    if (empty($url)) {
+      $url = getenv('JAWSDB_MARIA_URL');
+    }
+    $dbparts = parse_url($url);
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+
+    $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    $sql = file_get_contents('../../populate_db/populate_data.sql');
+    $stmt = $db->prepare($sql);
+    if ($stmt->execute()) {
+      echo "Successfully populated database with default data";
+    }
+    else {
+      echo "Failed to populate database";
+    }
+  }
+  else {
+    echo "Not allowed!";
+  }
+
+?>
